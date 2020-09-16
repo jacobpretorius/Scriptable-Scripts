fcedae3d5b7333d627018626f72389a63c8293eb
Jacob Pretorius <jacob@jcpretorius.com>
2020-09-16 18:30:03 +0100

    Use unified TodayView function

--- Today-View.js
+++ Today-View.js
@@ -5,41 +5,35 @@
 // Toggl time tracking for reading
 const togglProjectId = 123456789; // the project you use to track your reading (or whatever you want the widget to show)
 
-// Runs using this Azure Function -> https://github.com/jacobpretorius/Azure-Functions/blob/master/src/TogglProjectTrackedCheck/index.js
+// Runs using this Azure Function -> https://github.com/jacobpretorius/Azure-Functions/blob/master/src/TodayView/index.js
 // Get the deployed URL from your Azure functions portal and modify below by adding &projectId=${togglProjectId} to the end
-const url = `https://something.azurewebsites.net/api/TogglProjectTrackedCheck?code=SECRET&projectId=${togglProjectId}`;
+const url = `https://something.azurewebsites.net/api/TodayView?code=SECRET&projectId=${togglProjectId}`;
 
 const req = new Request(url);
 const result = await req.loadJSON();
-const readMinutes = Math.round(result.trackedDurationInMs / 60);
-
-
-// Todoist tasks
-// Runs using this Azure Function -> https://github.com/jacobpretorius/Azure-Functions/blob/master/src/TodoistGetItemsToday/index.js
-// Get the deployed URL from your Azure functions portal and paste it below
-const urlTodos = 'https://something.azurewebsites.net/api/TodoistGetItemsToday?code=SECRET';
-const reqTodos = new Request(urlTodos);
-const resultTodos = await reqTodos.loadJSON();
 
 if (config.runsInWidget) {
   // create and show widget
-  let colour = readMinutes >= 30 ? '#41cb86' : '#33C6FF'; // the 30 here is my reading goal in minutes
-  let dueNote = resultTodos.Count > 0
-    ? resultTodos.Count.toString()
-    : 'None ✌️';
-  let widget = createWidget("Read", `${readMinutes} m`, "Tasks due", dueNote, colour);
+  let colour = result.Toggl.MinutesTracked >= 30 ? '#41cb86' : '#33C6FF';
+  let dueNote = result.Todoist.Count > 0
+    ? result.Todoist.Count.toString()
+    : 'Zero :v:';
+
+  let widget = createWidget("Read", `${result.Toggl.MinutesTracked} m`, "Tasks due", dueNote, colour);
+  
   Script.setWidget(widget);
   Script.complete();
 } else {
   if (config.runsWithSiri){
-    Speech.speak(`Today you have ${resultTodos.Count} tasks and you've read ${readMinutes} minutes.`);
+    Speech.speak(`Today you have ${result.Todoist.Count} tasks and you've read ${result.Toggl.MinutesTracked} minutes.`);
   } else {
     let alert = new Alert();
     alert.title = 'Today';
-    alert.message = `${resultTodos.Count} tasks due and read ${readMinutes} Minutes.`;
+    alert.message = `${result.Todoist.Count} tasks due and read ${result.Toggl.MinutesTracked} Minutes.`;
     alert.presentAlert();
   }
 }
+
 function createWidget(preRead, read, preTodo, todos, color) {
   let widget = new ListWidget();
   widget.backgroundColor = new Color(color);
@@ -66,7 +60,6 @@ function createWidget(preRead, read, preTodo, todos, color) {
   preReadTxt.textColor = Color.white();
   preReadTxt.textOpacity = 0.8;
   preReadTxt.textSize = 14;
-  
   let readTxt = widget.addText(read);
   readTxt.textColor = Color.white();
   readTxt.textSize = 23;
